b57cf2033c0ddacc36dca8064e05af83a9001771
Davidht77 <davidandreshuette@gmail.com>
2025-05-07 23:49:42 -0500

    nest js done

--- backend/Typescript/microservice-3/src/app.module.ts
+++ backend/Typescript/microservice-3/src/app.module.ts
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ClientModule } from './client/client.module';
-import { EmployeeModule } from './employee/employee.module';
-@Module({
-  imports: [MongooseModule.forRoot('mongodb://mongoadmin:mongopassword@localhost:27017/mydatabase?authSource=admin'), ClientModule, EmployeeModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
-=======
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -26,7 +12,14 @@ import { ClientController } from './client/client.controller';
 import { ClientService } from './client/client.service';
 import { FeedbackModule } from './feedback/feedback.module';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongoadmin:mongopassword@localhost:27017/mydatabase?authSource=admin'), 
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     ClientModule, 
     ConfigModule.forRoot({ // Configura ConfigModule aquí
       isGlobal: true, // Esto hace que ConfigService esté disponible en cualquier módulo sin importarlo explícitamente
@@ -37,4 +30,3 @@ import { FeedbackModule } from './feedback/feedback.module';
   providers: [AppService, S3Service, ConfigService, ClientService],
 })
 export class AppModule {}
->>>>>>> e89bb9713ec89be9d22a9e827a80c2545fceff7d
